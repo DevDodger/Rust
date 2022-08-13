4cb05d4070122b38e9e92a0b522b10ecbbb7cbb3
DevDodger <reidlmiller@gmail.com>
2022-08-13 07:11:16 -0700

    a

---
+++ cargo_testing/target/debug/deps/cargo_testing-254efe4d2aa49eaa.d
@@ -0,0 +1,5 @@
+c:\Users\reidl\Documents\LearningRS\Rust\cargo_testing\target\debug\deps\cargo_testing-254efe4d2aa49eaa.rmeta: src\main.rs
+
+c:\Users\reidl\Documents\LearningRS\Rust\cargo_testing\target\debug\deps\cargo_testing-254efe4d2aa49eaa.d: src\main.rs
+
+src\main.rs:
---
+++ cargo_testing/target/debug/deps/cargo_testing-ed84e10bf309dd70.d
@@ -0,0 +1,5 @@
+c:\Users\reidl\Documents\LearningRS\Rust\cargo_testing\target\debug\deps\cargo_testing-ed84e10bf309dd70.rmeta: src\main.rs
+
+c:\Users\reidl\Documents\LearningRS\Rust\cargo_testing\target\debug\deps\cargo_testing-ed84e10bf309dd70.d: src\main.rs
+
+src\main.rs:
