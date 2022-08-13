c45f23d7cf462d1e500264a26ac211f37cdac225
DevDodger <reidlmiller@gmail.com>
2022-08-12 17:45:39 -0700

    a

---
+++ cargo_testing/target/debug/cargo_testing.d
@@ -0,0 +1,1 @@
+C:\Users\reidl\Documents\LearningRS\Rust\cargo_testing\target\debug\cargo_testing.exe: C:\Users\reidl\Documents\LearningRS\Rust\cargo_testing\src\main.rs
---
+++ cargo_testing/target/debug/deps/cargo_testing.d
@@ -0,0 +1,5 @@
+C:\Users\reidl\Documents\LearningRS\Rust\cargo_testing\target\debug\deps\cargo_testing.exe: src\main.rs
+
+C:\Users\reidl\Documents\LearningRS\Rust\cargo_testing\target\debug\deps\cargo_testing.d: src\main.rs
+
+src\main.rs:
